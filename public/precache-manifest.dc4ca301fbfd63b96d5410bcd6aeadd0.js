532299e9e2e3c0613a3ac5b9e75ed9e50091d2b3
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-06-27 18:22:12 +0530

    refactor

---
+++ public/precache-manifest.dc4ca301fbfd63b96d5410bcd6aeadd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54276bd5ad8bcfee13cff2aacc5ad52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fadae052ba4570a94017",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e3e39b2e31727c0157c1",
+    "url": "/static/js/2.127ffacf.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.127ffacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fadae052ba4570a94017",
+    "url": "/static/js/main.0a69cb11.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
